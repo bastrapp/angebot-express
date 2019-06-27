41bf24ec64284586dc4a3b3596b5a08bcf4397be
Guido Grogger <guido@grogger.de>
2019-06-27 15:05:07 +0200

    marktueberblick erstes design

--- chrome/immoscout_show_zinscheck.js
+++ chrome/immoscout_show_zinscheck.js
@@ -1,16 +1,36 @@
+var bankImages = { 
+    "ING" : "https://www.interhyp.de/home/ihyp/resources/ihyp/images/providerLogos/ing.png",
+    "Commerzbank AG" : "https://www.interhyp.de/home/ihyp/resources/ihyp/images/providerLogos/commerzbank.png",
+    "Allianz Lebensversicherungs AG" : "https://www.interhyp.de/home/ihyp/resources/ihyp/images/providerLogos/allianz.png"
+}
 
 function offer_html(offer) {
- return "<div style='display: table-row'>" + 
-           "<div style='display: table-cell;'>"+ offer.bankDetails.bankName + "</div>" +
-           "<div style='display: table-cell;'>"  +offer.effectiveInterest + "</div>" +
-	"</div>";
+
+
+  var bankName = offer.bankDetails.bankName;
+
+ var image = bankImages[offer.bankDetails.bankName];
+    console.log("img: " + image);
+ if(image) {
+   console.log("bid anpassen: " + image);
+
+    bankName = "<img src='"+image+"' width='130' height='50' />";
+ } else {
+  //bankName = bankName.substr(0,7);
+}
+
+ return "<tr >" + 
+           "<td>"+ bankName + "</td>" +
+           "<td style='font-size:16pt; width:70px' >"  +offer.effectiveInterest + "% </td>" +
+           "<td style='font-size:16pt;'>"  +offer.monthlyPayment  +"€</td>" +
+	"</tr>";
 }
 
 function show_zinscheck(result) {
 var offers = JSON.parse(result);
 
 var offer= offers[0];
-var offerHtml = offer_html(offers[0]) +  offer_html(offers[1]) +  offer_html(offers[2]) ;
+var offerHtml = "<table>" + offer_html(offers[0]) +  offer_html(offers[1]) +  offer_html(offers[2]) + "</table>";
 
 console.log(offer);
 
