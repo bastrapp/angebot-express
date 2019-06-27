e706b038edc688d5dd5c700428397e2f0f9f2337
bas <sebastian.trapp@gmail.com>
2019-06-27 11:03:27 +0200

    Modifying the dom

--- chrome/immoscout_show_zinscheck.js
+++ chrome/immoscout_show_zinscheck.js
@@ -1,2 +1,7 @@
 
-alert ("hallo")
+
+//var box = document.getElementsByClassName('is24-function-box')[0]
+var box = document.getElementById('is24-banner_right_1')
+
+
+box.innerHTML = "<p>GOT YOU!</p>"
